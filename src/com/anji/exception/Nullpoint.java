05c9e6ce017c838237be788aa98d84d081f2677b
gopalaanji <gopala.anji@gmail.com>
2018-11-04 18:18:16 +0530

    modified

--- src/com/anji/exception/Nullpoint.java
+++ src/com/anji/exception/Nullpoint.java
@@ -7,25 +7,27 @@ public class Nullpoint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = null;
-		/*
-		 * if (str.equals("java")) { //NullPointerException
-		 * System.out.println("yes! str equals to java"); } else {
-		 * System.out.print("str not equals to java"); }
-		 */
 
-		/*
-		 * StringBuffer buffer = null; if("Suneel".equals("Suneel")){
-		 * buffer.append("Kumar"); } System.out.println(buffer); }
-		 */
+		if (str.equals("java")) { // NullPointerException
+			System.out.println("yes! str equals to java");
+		} else {
+			System.out.print("str not equals to java");
+		}
 
-		// TODO Auto-generated method stub
 		StringBuffer buffer = null;
 		if ("Suneel".equals("Suneel")) {
-			buffer = new StringBuffer();
 			buffer.append("Kumar");
 		}
 		System.out.println(buffer);
 	}
+
+	// TODO Auto-generated method stub
+//		StringBuffer buffer = null;
+//		if ("Suneel".equals("Suneel")) {
+//			buffer = new StringBuffer();
+//			buffer.append("Kumar");
+//		}
+	// System.out.println(buffer);
 
 	/*
 	 * Object obj1 = new Object(); System.out.println(obj1.hashCode());
